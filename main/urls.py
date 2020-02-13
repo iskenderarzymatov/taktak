cb33f0bad289d4e01a8dd117bf3f3a1a3fbedcb1
isi <iskenderarzymatov>
2020-02-14 01:45:31 +0600

    links pages

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,17 @@ from django.urls import path
 
 from .views import *
 
+
+
+
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('about/', AboutView.as_view(), name='about'),
+    path('deyatenost/', DeyatelnostView.as_view(), name='deyatelnost'),
+    path('news/', NewsView.as_view(), name='news'),
+    path('partners/', PartnersView.as_view(), name='partners'),
+    path('tours/', ToursView.as_view(), name='tours'),
+    path('contactus/', ContactusView.as_view(), name='contactus'),
+
+
 ]
--- main/views.py
+++ main/views.py
@@ -27,3 +27,70 @@ class AboutView(TemplateView):
     def post(self, request, *args, **kwargs):
         return render(request, self.template_name, context={
         })
+
+
+class DeyatelnostView(TemplateView):
+    template_name = 'deyatelnost.html'
+
+    def get(self, request, *args, **kwargs):
+        # tours = Tour.objects.all()
+        return render(request, self.template_name, context={
+            # 'tours': tours,
+        })
+
+    def post(self, request, *args, **kwargs):
+        return render(request, self.template_name, context={
+        })
+
+
+class NewsView(TemplateView):
+    template_name = 'news.html'
+
+    def get(self, request, *args, **kwargs):
+        # tours = Tour.objects.all()
+        return render(request, self.template_name, context={
+            # 'tours': tours,
+        })
+
+    def post(self, request, *args, **kwargs):
+        return render(request, self.template_name, context={
+        })
+
+class PartnersView(TemplateView):
+    template_name = 'partners.html'
+
+    def get(self, request, *args, **kwargs):
+        # tours = Tour.objects.all()
+        return render(request, self.template_name, context={
+            # 'tours': tours,
+        })
+
+    def post(self, request, *args, **kwargs):
+        return render(request, self.template_name, context={
+        })
+
+class ToursView(TemplateView):
+    template_name = 'tours.html'
+
+    def get(self, request, *args, **kwargs):
+        # tours = Tour.objects.all()
+        return render(request, self.template_name, context={
+            # 'tours': tours,
+        })
+
+    def post(self, request, *args, **kwargs):
+        return render(request, self.template_name, context={
+        })
+
+class ContactusView(TemplateView):
+    template_name = 'contactus.html'
+
+    def get(self, request, *args, **kwargs):
+        # tours = Tour.objects.all()
+        return render(request, self.template_name, context={
+            # 'tours': tours,
+        })
+
+    def post(self, request, *args, **kwargs):
+        return render(request, self.template_name, context={
+        })
